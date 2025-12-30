7fa3f5971050a8a5eb81588b8191523903d48fd3
okangure60-cpu <okangure60@gmail.com>
2025-12-30 15:34:03 +0300

    refactor: Simplify mob data structure
    Removes ability definitions from mob templates and cleans up comments.
    This simplifies the mob data and prepares for future ability system rework.

--- constants.ts
+++ constants.ts
@@ -8,7 +8,6 @@ export const RARITY_COLORS: Record<ItemRarity, string> = {
   SUN: '#f97316',
 };
 
-// Resale values as percentage of level-based base value
 export const ITEM_RESALE_MULTIPLIERS: Record<ItemRarity, number> = {
   COMMON: 0.1,
   STAR: 0.3,
@@ -57,23 +56,22 @@ export const SRO_SKILLS: Skill[] = [
 
 export const SRO_MOBS: MobTemplate[] = [
   { id: 'mangyang', name: "Mangyang", lvl: 1, hp: 80, atk: 12, img: "https://i.ibb.co/8mR0oXz/mangyang.png", xpReward: 0.5, goldReward: 15 },
-  { id: 'movoi', name: "Movoi", lvl: 2, hp: 120, atk: 18, img: "https://i.ibb.co/YV7Nf2S/smalleye.png", xpReward: 1, goldReward: 25, abilities: [{ type: 'CHARGE', chance: 0.2, cooldown: 8000 }] },
+  { id: 'movoi', name: "Movoi", lvl: 2, hp: 120, atk: 18, img: "https://i.ibb.co/YV7Nf2S/smalleye.png", xpReward: 1, goldReward: 25 },
   { id: 'smalleye', name: "Small Eye", lvl: 3, hp: 180, atk: 28, img: "https://i.ibb.co/YV7Nf2S/smalleye.png", xpReward: 1.5, goldReward: 40 },
-  { id: 'bigeye', name: "Big Eye", lvl: 4, hp: 250, atk: 35, img: "https://i.ibb.co/YV7Nf2S/smalleye.png", xpReward: 2, goldReward: 60, abilities: [{ type: 'CHARGE', chance: 0.25, cooldown: 10000 }] },
   { id: 'oldweasel', name: "Old Weasel", lvl: 5, hp: 320, atk: 45, img: "https://i.ibb.co/mS9X3J1/tiger.png", xpReward: 2.5, goldReward: 80 },
-  { id: 'weasel', name: "Weasel", lvl: 6, hp: 400, atk: 55, img: "https://i.ibb.co/mS9X3J1/tiger.png", xpReward: 3, goldReward: 110, abilities: [{ type: 'CHARGE', chance: 0.3, cooldown: 12000 }] },
-  { id: 'waterghost', name: "Water Ghost", lvl: 7, hp: 1200, atk: 120, img: "https://i.ibb.co/LhB2PzF/waterghost.png", xpReward: 15, goldReward: 150, abilities: [{ type: 'POISON', chance: 0.3, cooldown: 8000 }] },
-  { id: 'waterghostslave', name: "Water Ghost Slave", lvl: 8, hp: 600, atk: 85, img: "https://i.ibb.co/LhB2PzF/waterghost.png", xpReward: 4, goldReward: 200 },
-  { id: 'hyena', name: "Hyena", lvl: 9, hp: 720, atk: 105, img: "https://i.ibb.co/N1pXW5j/wolf.png", xpReward: 4.5, goldReward: 260, abilities: [{ type: 'STUN', chance: 0.15, cooldown: 20000 }] },
-  { id: 'hyenahound', name: "Hyena Hound", lvl: 10, hp: 850, atk: 125, img: "https://i.ibb.co/N1pXW5j/wolf.png", xpReward: 5, goldReward: 350, abilities: [{ type: 'CHARGE', chance: 0.3, cooldown: 15000 }] },
-  { id: 'yeowa', name: "Decayed Yeowa", lvl: 11, hp: 1000, atk: 155, img: "https://i.ibb.co/VvzK9R1/chakji.png", xpReward: 5.5, goldReward: 480, abilities: [{ type: 'POISON', chance: 0.3, cooldown: 15000 }] },
-  { id: 'yeowa_full', name: "Yeowa", lvl: 12, hp: 1200, atk: 190, img: "https://i.ibb.co/VvzK9R1/chakji.png", xpReward: 6, goldReward: 620, abilities: [{ type: 'STUN', chance: 0.2, cooldown: 20000 }] },
-  { id: 'tiger', name: "Tiger", lvl: 13, hp: 5000, atk: 350, img: "https://i.ibb.co/mS9X3J1/tiger.png", xpReward: 50, goldReward: 800, abilities: [{ type: 'CHARGE', chance: 0.5, cooldown: 8000 }, { type: 'STUN', chance: 0.2, cooldown: 15000 }] },
-  { id: 'tigerslave', name: "Tiger Slave", lvl: 14, hp: 1800, atk: 280, img: "https://i.ibb.co/mS9X3J1/tiger.png", xpReward: 7, goldReward: 1000 },
-  { id: 'banditarcher', name: "Bandit Archer", lvl: 15, hp: 2200, atk: 340, img: "https://i.ibb.co/0XW5g9s/niya.png", xpReward: 7.5, goldReward: 1300, abilities: [{ type: 'CHARGE', chance: 0.35, cooldown: 10000 }] },
-  { id: 'bandit', name: "Bandit", lvl: 16, hp: 2600, atk: 410, img: "https://i.ibb.co/0XW5g9s/niya.png", xpReward: 8, goldReward: 1700, abilities: [{ type: 'STUN', chance: 0.2, cooldown: 15000 }] },
-  { id: 'chakji', name: "Chakji", lvl: 18, hp: 3500, atk: 550, img: "https://i.ibb.co/VvzK9R1/chakji.png", xpReward: 9, goldReward: 2500, abilities: [{ type: 'POISON', chance: 0.4, cooldown: 10000 }] },
-  { id: 'ghostsungsung', name: "Ghost SungSung", lvl: 20, hp: 15000, atk: 850, img: "https://i.ibb.co/r7v9t7H/earthghost.png", xpReward: 150, goldReward: 4000, abilities: [{ type: 'CHARGE', chance: 0.5, cooldown: 10000 }, { type: 'STUN', chance: 0.3, cooldown: 15000 }, { type: 'POISON', chance: 0.2, cooldown: 20000 }] },
+  { id: 'waterghost', name: "Water Ghost", lvl: 7, hp: 1200, atk: 120, img: "https://i.ibb.co/LhB2PzF/waterghost.png", xpReward: 15, goldReward: 150 },
+  { id: 'hyena', name: "Hyena", lvl: 9, hp: 720, atk: 105, img: "https://i.ibb.co/N1pXW5j/wolf.png", xpReward: 4.5, goldReward: 260 },
+  { id: 'yeowa', name: "Yeowa", lvl: 12, hp: 1200, atk: 190, img: "https://i.ibb.co/VvzK9R1/chakji.png", xpReward: 6, goldReward: 620 },
+  { id: 'tiger', name: "Tiger", lvl: 13, hp: 5000, atk: 350, img: "https://i.ibb.co/mS9X3J1/tiger.png", xpReward: 50, goldReward: 800 },
+  { id: 'bandit', name: "Bandit", lvl: 16, hp: 2600, atk: 410, img: "https://i.ibb.co/0XW5g9s/niya.png", xpReward: 8, goldReward: 1700 },
+  { id: 'chakji', name: "Chakji", lvl: 18, hp: 3500, atk: 550, img: "https://i.ibb.co/VvzK9R1/chakji.png", xpReward: 9, goldReward: 2500 },
+  { id: 'ghostsungsung', name: "Ghost SungSung", lvl: 20, hp: 15000, atk: 850, img: "https://i.ibb.co/r7v9t7H/earthghost.png", xpReward: 150, goldReward: 4000 },
+  { id: 'ishade', name: "Ishade", lvl: 25, hp: 8500, atk: 1200, img: "https://i.ibb.co/LhB2PzF/waterghost.png", xpReward: 15, goldReward: 5000 },
+  { id: 'penon', name: "Penon Warrior", lvl: 28, hp: 11000, atk: 1600, img: "https://i.ibb.co/r7v9t7H/earthghost.png", xpReward: 18, goldReward: 7500 },
+  { id: 'uruchi', name: "Uruchi", lvl: 30, hp: 50000, atk: 3500, img: "https://i.ibb.co/mS9X3J1/tiger.png", xpReward: 500, goldReward: 25000 },
+  { id: 'niyasniper', name: "Niya Sniper", lvl: 35, hp: 18000, atk: 2500, img: "https://i.ibb.co/0XW5g9s/niya.png", xpReward: 35, goldReward: 15000 },
+  { id: 'isyutaru', name: "Isyutaru", lvl: 40, hp: 150000, atk: 8000, img: "https://i.ibb.co/LhB2PzF/waterghost.png", xpReward: 2000, goldReward: 100000 },
+  { id: 'lordyarkan', name: "Lord Yarkan", lvl: 50, hp: 500000, atk: 25000, img: "https://i.ibb.co/VvzK9R1/chakji.png", xpReward: 10000, goldReward: 500000 },
 ];
 
 export const SRO_DUNGEONS: DungeonTemplate[] = [
@@ -82,36 +80,72 @@ export const SRO_DUNGEONS: DungeonTemplate[] = [
     name: 'Unutulmuş Mağara',
     minLvl: 5,
     waves: 5,
-    mobPool: ['smalleye', 'bigeye', 'oldweasel'],
+    mobPool: ['smalleye', 'oldweasel'],
     bossId: 'waterghost',
-    goldReward: 5000,
-    entryFee: 1000,
+    goldReward: 35000,
+    entryFee: 10000,
     specialDropRate: 0.15,
-    description: 'Karanlık ve nemli bir mağara. Giriş için 1.000 Altın gerekir.'
+    description: 'Karanlık ve nemli bir mağara. İlk büyük sınavın burada başlıyor.'
   },
   {
     id: 'bandit_fortress',
     name: 'Bandit Kalesi',
     minLvl: 12,
     waves: 8,
-    mobPool: ['banditarcher', 'bandit', 'yeowa'],
+    mobPool: ['yeowa', 'bandit'],
     bossId: 'tiger',
-    goldReward: 15000,
-    entryFee: 5000,
+    goldReward: 90000,
+    entryFee: 30000,
     specialDropRate: 0.2,
-    description: 'Eşkıyaların kalesi. Giriş için 5.000 Altın gerekir.'
+    description: 'Eşkıyaların kalesi. İçerideki altınlar girişte ödediğinden çok daha fazlası!'
+  },
+  {
+    id: 'tarim_basin',
+    name: 'Tarim Havzası',
+    minLvl: 18,
+    waves: 10,
+    mobPool: ['chakji', 'bandit'],
+    bossId: 'ghostsungsung',
+    goldReward: 250000,
+    entryFee: 75000,
+    specialDropRate: 0.25,
+    description: 'Kadim ruhların koruduğu bereketli topraklar.'
+  },
+  {
+    id: 'karakoram_glade',
+    name: 'Karakoram Buzulu',
+    minLvl: 25,
+    waves: 12,
+    mobPool: ['ishade', 'penon'],
+    bossId: 'uruchi',
+    goldReward: 750000,
+    entryFee: 200000,
+    specialDropRate: 0.35,
+    description: 'Dondurucu soğuk ve acımasız canavarlar. Uruchi seni bekliyor.'
   },
   {
     id: 'taklamakan_tomb',
-    name: 'Taklamakan Mezarı',
-    minLvl: 18,
-    waves: 10,
-    mobPool: ['chakji', 'yeowa_full', 'hyenahound'],
-    bossId: 'ghostsungsung',
-    goldReward: 50000,
-    entryFee: 25000,
-    specialDropRate: 0.25,
-    description: 'Kadim ruhların mezarı. Giriş için 25.000 Altın gerekir.'
+    name: 'Karakoram Zirvesi',
+    minLvl: 35,
+    waves: 15,
+    mobPool: ['niyasniper', 'penon'],
+    bossId: 'isyutaru',
+    goldReward: 1800000,
+    entryFee: 500000,
+    specialDropRate: 0.45,
+    description: 'Buz Kraliçesi Isyutaru nun meskeni. Sadece en güçlüler sağ çıkabilir.'
+  },
+  {
+    id: 'kings_valley',
+    name: 'Krallar Vadisi',
+    minLvl: 45,
+    waves: 20,
+    mobPool: ['niyasniper', 'ishade'],
+    bossId: 'lordyarkan',
+    goldReward: 4500000,
+    entryFee: 1000000,
+    specialDropRate: 0.6,
+    description: 'Silkroad ın gerçek hükümdarı Lord Yarkan ile yüzleşin. Servet ve şan burada!'
   }
 ];
 
